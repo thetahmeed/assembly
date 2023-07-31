b60a7d9a23292a161d9360cdde5779638c004729
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2023-07-31 21:47:23 +0600

    Create a_comment.asm

---
+++ a_comment.asm
@@ -0,0 +1,18 @@
+; Hello! 
+; This is our first progeam
+
+; Here you can see in the starting of the every line I am
+; using ';' semi-colol,  this is used to write a comment.
+
+
+
+
+; Softwere we using called 'emu8086'
+; Download from here: https://emu8086-microprocessor-emulator.en.softonic.com/download
+
+
+; Open the app > click on new > empty workshop > write codes > save/commpile
+
+; That't it! :)   
+
+; (Maybe :p)              
