6c9e62f84ccf58c7705f5da4312b74dafaa9cf3a
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2023-07-31 22:42:26 +0600

    Create d_basic_structure.asm

---
+++ d_basic_structure.asm
@@ -0,0 +1,12 @@
+; basic structure
+
+.model small        ; Program uses a maximum of 64k of memory for code
+.data               ; Biginning of the data segment where variable stored
+.code               ; Biginning of the code segment
+main proc
+          
+          ; we will write codes here   
+          mov al, 9
+    
+endp
+end main
